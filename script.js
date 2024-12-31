2781ebe844495a979e69c32eae8b5a9229e2b290
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-31 20:42:07 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Typing animation
 // ****************************** //
-const typed = document.getElementById('typed');
+const typed = document.querySelector('.typed');
 const strings = ["Hello There ...", "","This is a typing animation example","Made By Abdessamad"]; // Array of strings to display
 
 const options = {
@@ -15,4 +15,18 @@ const options = {
   contentType: 'html', // Allow HTML tags in strings for richer content
 };
 const typedInstance = new Typed(typed, options);
+// ****************************** //
+// Smooth animation cards
+// ****************************** //
+anime({
+  targets: '.child', // Select multiple elements
+  translateX: [-100, 0],
+  opacity: [0, 1],
+  delay: anime.stagger(500), // Stagger the delay by 100ms
+  duration: 500,
+  direction: 'alternate',
+  loop: true,
+
+  easing: 'easeOutQuad'
+});
 // ****************************** //
